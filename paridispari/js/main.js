a2257862951a5ddae5657c32adbd0aceb6b3e950
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-08 17:01:24 +0200

    END PARI-DISPARI

--- paridispari/js/main.js
+++ paridispari/js/main.js
@@ -5,6 +5,7 @@ start.addEventListener("click",
     function(){
         
         // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+        
         // prompt("PARI o DISPARI")
         let userchoice = document.getElementById("choice").value
         console.log(userchoice);
@@ -14,36 +15,42 @@ start.addEventListener("click",
         console.log(userNumber);
 
         // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+        
+        function randomNumber (min, max) {
+            let pcNumber = Math.floor(Math.random() * (max - min)) + min;
+            return pcNumber
+        }
+        
         let pcNumber = randomNumber (1, 5);
         console.log(pcNumber);
         
-
         // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+        
+        function paridisparivincitore (user, pc) {
+            let esito = user + pc;
+            if(esito % 2 === 0 && userchoice === "PARI") {
+                return "Hai vinto"
+            }
+            
+            else if (esito % 2 === 1 && userchoice === "DISPARI") {
+                return "Hai vinto"
+            }
+            
+            else {
+                return "Hai perso"
+            }
+            
+        }
+        
         let vincitore = paridisparivincitore(userNumber, pcNumber) ;
         console.log(vincitore);
-        
+
         let answer = document.getElementById("answer");
         answer.innerHTML = vincitore;
-        
+
+
     }
-    )
-    
-    
-    // dichiarazione funzioni
-    
-    function randomNumber (min, max) {
-        let pcNumber = Math.floor(Math.random() * (max - min)) + min;
-        return pcNumber
-    }
-    
-    function paridisparivincitore (user, pc) {
-        let esito = user + pc;
-        if(esito % 2 === 0 && userchoice === "PARI") {
-            return "Hai vinto"
-        }
-        
-        else {
-            return "Hai perso"
-        }
-        
-    }
+)
+
+
+
