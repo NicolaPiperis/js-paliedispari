f3e1a0b82c6e687bc7822605c23e1e6ffe5dd3f3
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-08 17:32:15 +0200

    log polindromo done

--- palindromo/js/main.js
+++ palindromo/js/main.js
@@ -4,8 +4,25 @@
 let userWord = prompt("Inserire una parola");
 console.log(userWord);
 
-let reverse = userWord.split("").reverse().join("");
+
+
+function myFunction (string) {
+
+    let reverse = userWord.split("").reverse().join("");
+    return reverse;
+
+}
+
+let reverse = myFunction(userWord);
 console.log(reverse);
 
+
+if(userWord === reverse) {
+    console.log("la parola è palindroma");
+} else {
+    console.log("la parola non è polindroma");
+}
+
 // Creare una funzione per capire se la parola inserita è palindroma    
+
 
